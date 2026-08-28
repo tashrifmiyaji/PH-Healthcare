cbb712303de0ae4fda78a22434e97670f9eca8e8
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-08-28 15:04:18 +0600

    feat: add Bkash integration for token management and configuration

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -33,5 +33,11 @@ export default {
 	email_sender : process.env.EMAIL_SENDER!,
 	cloudinary_cloud_name : process.env.CLOUDINARY_CLOUD_NAME!,
 	cloudinary_api_key : process.env.CLOUDINARY_API_KEY!,
-	cloudinary_api_secret : process.env.CLOUDINARY_API_SECRET!
+	cloudinary_api_secret : process.env.CLOUDINARY_API_SECRET!,
+	bkash_base_url : process.env.BKASH_BASE_URL!,
+	bkash_username : process.env.BKASH_USERNAME!,
+	bkash_password : process.env.BKASH_PASSWORD!,
+	bkash_app_key : process.env.BKASH_APP_KEY!,
+	bkash_app_secret : process.env.BKASH_APP_SECRET!,
+	bkash_callback_url: process.env.BKASH_CALLBACK_URL!,
 };
